d68f795db2216e9c5a2d5151fcfa004ac98da8d5
Marc-André Bühler <git@public.mabsoftware.ch>
2013-10-26 18:05:25 +0200

    Fixed return type in ICPU

--- src/ch/minipowerpcemulator/ICPU.java
+++ src/ch/minipowerpcemulator/ICPU.java
@@ -5,7 +5,7 @@ public interface ICPU {
 	 * Executes a single instruction by reading the opcode from the instruction register,
 	 * interpreting and executing the opcode.
 	 */
-	boolean executeSingle();
+	void executeSingle();
 	public short getProgramCounter();
 	public void setProgramCounter(short programCounter);
 	public short getInstructionRegister();
