355d07c05cb9a7b4a61555659880075cb980027f
Marc-André Bühler <git@public.mabsoftware.ch>
2013-10-26 18:03:10 +0200

    Added missing methods to ICPU

--- src/ch/minipowerpcemulator/ICPU.java
+++ src/ch/minipowerpcemulator/ICPU.java
@@ -5,5 +5,8 @@ public interface ICPU {
 	 * Executes a single instruction by reading the opcode from the instruction register,
 	 * interpreting and executing the opcode.
 	 */
-	void executeSingle();
+	boolean executeSingle();
+	public short getProgramCounter();
+	public void setProgramCounter(short programCounter);
+	public short getInstructionRegister();
 }
