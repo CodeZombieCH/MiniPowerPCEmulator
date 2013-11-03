5ce7756f363c431e0326a3ad04b23d400a8af375
samfisch3r <sam@dienachbarn.ch>
2013-11-03 13:32:00 +0100

    Working Version of multiplication also with negative numbers as input

--- programs/multi_overflow.asm
+++ programs/multi_overflow.asm
@@ -1,5 +1,5 @@
-@500 125
-@502 8000
+@500 5000
+@502 5000
 
 CLR R0			//Akku = 0
 INC				//Akku = 1
@@ -9,69 +9,47 @@ ADDD #32767		//Akku = -32768
 SWDD R0, #512	//Addr 512 = -32768
 LWDD R2, #512	//R2 = -32768
 LWDD R0, #500	//Akku = Number1
+AND R2			//Bitmask pos/neg
+SWDD R0, #510	//Addr 510 = Bitmask Number1
+BZD LONEEV		//Jump if Number1 positive
+LWDD R0, #500	//Akku = Number1
+DEC				//Akku -1
+NOT				//Invert Akku
+SWDD R0, #500	//Addr 500 = Number1 positive
+LONEEV:
+LWDD R0, #502	//Akku = Number2
+AND R2			//Bitmask pos/neg
+BZD LTOWEV		//Jump if Number2 positive
+LWDD R1, #510	//R1 = Bitmask Number1
+AND R1			//Bitmask R1
+NOT				//Invert Akku
+AND R2			//Bitmask R2
+SWDD R0, #510	//Addr 510 = Result pos/neg
+LWDD R0, #502	//Akku = Number2
+DEC				//Akku -1
+NOT				//Invert Akku
+SWDD R0, #502	//Addr 502 = Number2 positive
+LTOWEV:
+LWDD R0, #500	//Akku = Number1
 LWHILE1:		
 BZD LEND		//while Number1 > 0 else Jump to End
 AND R3			//Bitmask Number1 & 1
 BZD LJPM1		//Jump to
 LWDD R0, #506	//Akku = Total
-SWDD R0, #518	//Addr 518 = Total
 LWDD R1, #502	//R1 = Number2
-ADD R1			//Akku = Akku + Number2
+ADD R1			//Akku = Akku + Number2 (Check carry flag)
 SWDD R0, #506	//Addr 506 = Total
-
-LWDD R0, #502	//Akku = Number2
-AND R2			//Bitmask pos/neg
-BZD LPOS1		//Jump if pos
-LWDD R0, #502	//Akku = Number2
-DEC				//Akku -1
-NOT				//Invert Akku
-SWDD R3, #520
-BD LJMP2		//Jump down
-LPOS1:
-LWDD R0, #502	//Akku = Number2
-LJMP2:
-SWDD R0, #516	//Addr 516 = positive Number2
-
-LWDD R0, #506	//Akku = Total
-AND R2			//Bitmask pos/neg
-BZD LPOS2		//Jump if pos
-LWDD R0, #506	//Akku = Total
-DEC				//Akku -1
-NOT				//Invert Akku
-SWDD R3, #522
-BD LJMP3		//Jump down
-LPOS2:
-LWDD R0, #506	//Akku = Total
-LJMP3:
-SWDD R0, #518
-
-LWDD R0, #520
-LWDD R1, #522
-AND R1
-BNZD LONE
-
-LWDD R1, #516	//R1 = positive Number2
-LWDD R0, #518
-BD LADD
-
-LONE:
-ADDD #32767
-BD LADDD
-
-LADD:
-ADD R1			//Akku = Akku + Total
-LADDD:
 LWDD R0, #504	//Akku = Result overflow
 LWDD R1, #514	//R1 = Overflow Number2
 BCD LNOR		//Overflow with number
 ADD R1			//Add Overflow Number2
-BCD LEND		//Overflow with number
+BCD LOVER		//Overflow with number
 BD LNRD			//Jump to Save
 LNOR:
 ADD R1			//Add Overflow Number2
-BCD LEND		//Overflow with number
+BCD LOVER		//Overflow with number
 INC				//Add overflow
-BCD LEND		//Overflow with number
+BCD LOVER		//Overflow with number
 LNRD:
 SWDD R0, #504	//Save overflow result
 LJPM1:
@@ -85,17 +63,34 @@ SWDD R0, #502	//Write back new Number2
 LWDD R0, #514	//Get overflow Number2
 BCD LNO2		//Overflow
 SLA				//Double overflow Number
-BCD LEND		//Overflow with number
+BCD LOVER		//Overflow with number
 BD LN2D			//Jump to Save
 LNO2:
 SLA				//Double overflow Number
-BCD LEND		//Overflow with number
+BCD LOVER		//Overflow with number
 INC				//Add overflow
-BCD LEND		//Overflow with number
+BCD LOVER		//Overflow with number
 LN2D:
 SWDD R0, #514	//Addr 514 = overflow Number2
 LWDD R0, #500	//Akku = Number1
 LIF1:
 BNZD LWHILE1	//If Number1 != 0 Jump back to while loop
 LEND:
+LWDD R0, #510	//Akku = Result pos/neg
+BZD LOVER		//If Result pos Jump to End
+LWDD R0, #506	//Akku = Result lower part
+BZD ZERO		//Jump If lower part is Zero
+DEC				//Akku -1
+NOT				//Invert Akku
+SWDD R0, #506	//Addr 506 = negative lower part of Result
+LWDD R0, #504	//Akku = Result higher part
+NOT				//Invert Akku
+BD LSAVE		//Jump to save
+ZERO:
+LWDD R0, #504	//Akku = Result higher part
+DEC				//Akku -1
+NOT				//Invert Akku
+LSAVE:
+SWDD R0, #504	//Addr 504 = Result higher part
+LOVER:
 END
