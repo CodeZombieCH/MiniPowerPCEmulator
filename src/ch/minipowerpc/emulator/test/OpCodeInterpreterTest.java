eba99d92bbb39fd8f6e59cff044ea050ecb00f0e
samfisch3r <sam@dienachbarn.ch>
2013-11-04 10:45:34 +0100

    Align test with opcode update

--- src/ch/minipowerpc/emulator/test/OpCodeInterpreterTest.java
+++ src/ch/minipowerpc/emulator/test/OpCodeInterpreterTest.java
@@ -12,10 +12,12 @@ import ch.minipowerpc.emulator.IALU;
 import ch.minipowerpc.emulator.IMemory;
 import ch.minipowerpc.emulator.OpCodeInterpreter;
 import ch.minipowerpc.emulator.Registers;
+import ch.minipowerpc.emulator.instructions.IInstruction;
 
 public class OpCodeInterpreterTest {
 	private OpCodeInterpreter instance;
 	private IALU alu;
+	private IInstruction instruction;
 
 	@Before
 	public void setUp() throws Exception {
@@ -27,183 +29,184 @@ public class OpCodeInterpreterTest {
 	@Test
 	public void testCLR() {
 		// Test CLR
-		instance.interpret((short) 0b0000001010000000);
+		instruction = instance.interpret((short) 0b0000001010000000);
 		assertEquals((short)0b00, instance.getRegister());
-		assertEquals("CLR 0", instance.getCommand());
+		assertEquals("CLR Accu", instruction.toString());
+		
 		// Test CLR
-		instance.interpret((short) 0b0000111011111111);
-		assertEquals((short)0b11, instance.getRegister());
-		assertEquals("CLR 3", instance.getCommand());
+		instruction = instance.interpret((short) 0b0000111011111111);
+		assertEquals((short)0b11, instance.getRegister());	
+		assertEquals("CLR R3", instruction.toString());
 	}
 	
 	@Test
 	public void testADD() {
 		// Test ADD
-		instance.interpret((short) 0b0000011110101010);
+		instruction = instance.interpret((short) 0b0000011110101010);
 		assertEquals((short)0b01, instance.getRegister());
-		assertEquals("ADD 1", instance.getCommand());
+		assertEquals("ADD R1", instruction.toString());
 	}
 	
 	@Test
 	public void testADDD() {
 		// Test ADDD
-		instance.interpret((short) 0b1000011111111111);
+		instruction = instance.interpret((short) 0b1000011111111111);
 		assertEquals((short)0b000011111111111, instance.getNumber());
-		assertEquals("ADDD 2047", instance.getCommand());
+		assertEquals("ADDD 2047", instruction.toString());
 	}
 	
 	@Test
 	public void testINC() {
 		// Test INC
-		instance.interpret((short) 0b0000000101010101);
-		assertEquals("INC", instance.getCommand());
+		instruction = instance.interpret((short) 0b0000000101010101);
+		assertEquals("INC", instruction.toString());
 	}
 	
 	@Test
 	public void testDEC() {
 		// Test DEC
-		instance.interpret((short) 0b0000010001010101);
-		assertEquals("DEC", instance.getCommand());
+		instruction = instance.interpret((short) 0b0000010001010101);
+		assertEquals("DEC", instruction.toString());
 	}
 	
 	@Test
 	public void testLWDD() {
 		// Test LWDD
-		instance.interpret((short) 0b0101100011001100);
+		instruction = instance.interpret((short) 0b0101100011001100);
 		assertEquals((short)0b10, instance.getRegister());
 		assertEquals((short)0b0011001100, instance.getAddress());
-		assertEquals("LWDD 2, 204", instance.getCommand());
+		assertEquals("LWDD R2, #204", instruction.toString());
 	}
 	
 	@Test
 	public void testSWDD() {
 		// Test SWDD
-		instance.interpret((short) 0b0111010010101100);
+		instruction = instance.interpret((short) 0b0111010010101100);
 		assertEquals((short)0b01, instance.getRegister());
 		assertEquals((short)0b0010101100, instance.getAddress());
-		assertEquals("SWDD 1, 172", instance.getCommand());
+		assertEquals("SWDD R1, #172", instruction.toString());
 	}
 	
 	@Test
 	public void testSRA() {
 		// Test SRA
-		instance.interpret((short) 0b0000010100101010);
-		assertEquals("SRA", instance.getCommand());
+		instruction = instance.interpret((short) 0b0000010100101010);
+		assertEquals("SRA", instruction.toString());
 	}
 	
 	@Test
 	public void testSLA() {
 		// Test SLA
-		instance.interpret((short) 0b0000100000101111);
-		assertEquals("SLA", instance.getCommand());
+		instruction = instance.interpret((short) 0b0000100000101111);
+		assertEquals("SLA", instruction.toString());
 	}
 	
 	@Test
 	public void testSRL() {
 		// Test SRL
-		instance.interpret((short) 0b0000100100101000);
-		assertEquals("SRL", instance.getCommand());
+		instruction = instance.interpret((short) 0b0000100100101000);
+		assertEquals("SRL", instruction.toString());
 		
 	}
 	
 	@Test
 	public void testSLL() {
 		// Test SLL
-		instance.interpret((short) 0b0000110001101010);
-		assertEquals("SLL", instance.getCommand());
+		instruction = instance.interpret((short) 0b0000110001101010);
+		assertEquals("SLL", instruction.toString());
 	}
 	
 	@Test
 	public void testAND() {
 		// Test AND
-		instance.interpret((short) 0b0000111000000000);
+		instruction = instance.interpret((short) 0b0000111000000000);
 		assertEquals((short)0b11, instance.getRegister());
-		assertEquals("AND 3", instance.getCommand());
+		assertEquals("AND R3", instruction.toString());
 	}
 
 	@Test
 	public void testOR() {
 		// Test OR
-		instance.interpret((short) 0b0000101100011000);
+		instruction = instance.interpret((short) 0b0000101100011000);
 		assertEquals((short)0b10, instance.getRegister());
-		assertEquals("OR 2", instance.getCommand());
+		assertEquals("OR R2", instruction.toString());
 	}
 
 	@Test
 	public void testNOT() {
 		// Test NOT
-		instance.interpret((short) 0b0000000010101010);
-		assertEquals("NOT", instance.getCommand());
+		instruction = instance.interpret((short) 0b0000000010101010);
+		assertEquals("NOT", instruction.toString());
 	}
 
 	@Test
 	public void testBZ() {
 		// Test BZ
-		instance.interpret((short) 0b0001011000011111);
+		instruction = instance.interpret((short) 0b0001011000011111);
 		assertEquals((short)0b01, instance.getRegister());
-		assertEquals("BZ 1", instance.getCommand());
+		assertEquals("BZ R1", instruction.toString());
 	}
 
 	@Test
 	public void testBNZ() {
 		// Test BNZ
-		instance.interpret((short) 0b0001100100011001);
+		instruction = instance.interpret((short) 0b0001100100011001);
 		assertEquals((short)0b10, instance.getRegister());
-		assertEquals("BNZ 2", instance.getCommand());	
+		assertEquals("BNZ R2", instruction.toString());	
 	}
 
 	@Test
 	public void testBC() {
 		// Test BC
-		instance.interpret((short) 0b0001111111111111);
+		instruction = instance.interpret((short) 0b0001111111111111);
 		assertEquals((short)0b11, instance.getRegister());
-		assertEquals("BC 3", instance.getCommand());
+		assertEquals("BC R3", instruction.toString());
 	}
 
 	@Test
 	public void testB() {
 		// Test B
-		instance.interpret((short) 0b0001000000000000);
+		instruction = instance.interpret((short) 0b0001000000000000);
 		assertEquals((short)0b00, instance.getRegister());
-		assertEquals("B 0", instance.getCommand());
+		assertEquals("B Accu", instruction.toString());
 	}
 
 	@Test
 	public void testBZD() {
 		// Test BZD
-		instance.interpret((short) 0b0011000000111000);
+		instruction = instance.interpret((short) 0b0011000000111000);
 		assertEquals((short)0b0000111000, instance.getAddress());
-		assertEquals("BZD 56", instance.getCommand());
+		assertEquals("BZD #56", instruction.toString());
 	}
 
 	@Test
 	public void testBNZD() {
 		// Test BNZD
-		instance.interpret((short) 0b0010110000111011);
+		instruction = instance.interpret((short) 0b0010110000111011);
 		assertEquals((short)0b0000111011, instance.getAddress());
-		assertEquals("BNZD 59", instance.getCommand());
+		assertEquals("BNZD #59", instruction.toString());
 	}
 
 	@Test
 	public void testBCD() {
 		// Test BCD
-		instance.interpret((short) 0b0011111111111111);
+		instruction = instance.interpret((short) 0b0011111111111111);
 		assertEquals((short)0b1111111111, instance.getAddress());
-		assertEquals("BCD 1023", instance.getCommand());
+		assertEquals("BCD #1023", instruction.toString());
 	}
 
 	@Test
 	public void testBD() {
 		// Test BD
-		instance.interpret((short) 0b0010000010101010);
+		instruction = instance.interpret((short) 0b0010000010101010);
 		assertEquals((short)0b0010101010, instance.getAddress());
-		assertEquals("BD 170", instance.getCommand());
+		assertEquals("BD #170", instruction.toString());
 	}
 
 	@Test
 	public void testEND() {
 		// Test END
-		instance.interpret((short) 0b0000000000000000);
-		assertEquals("END", instance.getCommand());
+		instruction = instance.interpret((short) 0b0000000000000000);
+		assertEquals("END", instruction.toString());
 	}
 }
